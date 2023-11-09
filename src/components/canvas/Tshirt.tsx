e6ec912a66d359201304c196caa3de5aaacfc1ad
eric valcik <eric.valcik@gmail.com>
2023-11-09 21:54:46 +0100

    rotate slower

--- src/components/canvas/Tshirt.tsx
+++ src/components/canvas/Tshirt.tsx
@@ -4,7 +4,7 @@ import { useFrame } from '@react-three/fiber'
 export function Tshirt(props) {
   const { scene } = useGLTF('/tshirt-centered.glb')
 
-  useFrame((state, delta) => (scene.rotation.y += delta))
+  useFrame((state, delta) => (scene.rotation.y += delta * 0.25))
 
   return <primitive object={scene} {...props} />
 }
